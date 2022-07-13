ee93257cd2bd450e718c7ca6e4b021301094b841
vishal-github12 <lahakarvishal123@gmail.com>
2022-07-13 16:58:44 +0530

    codeRefactor

---
+++ planmytrip-ms/src/main/java/com/example/planmytripms/PlanmytripMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.planmytripms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanmytripMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanmytripMsApplication.class, args);
+	}
+
+}
